879a2af1fbd1bf40e7ac8777f7e39656d5de67fe
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-03-12 23:51:56 +0200

    Added 8 Queen puzzle solution.

---
+++ 01.Recursion, Sorting and Searching Algorithms/Lab/05. Generating Combinations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Generating_Combinations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var set = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var vectorLenght = int.Parse(Console.ReadLine());
+            var vector = new int[vectorLenght];
+            GenCombinations(set,vector,0,0);
+        }
+
+        public static void GenCombinations(int[] set, int[] vector, int vectorIndex, int border)
+        {
+            if (vector.Length == vectorIndex)
+            {
+                Console.WriteLine(string.Join(" ",vector));
+            }
+            else
+            {
+                for (int i = border; i < set.Length; i++)
+                {
+                    vector[vectorIndex] = set[i];
+                    GenCombinations(set,vector,vectorIndex + 1, i + 1);
+                }
+            }
+        }
+    }
+}
---
+++ 01.Recursion, Sorting and Searching Algorithms/Lab/06. Queens Puzzle/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Queens_Puzzle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var matrix = new int[8][];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new int[8];
+            }
+
+            PutQueens(matrix,0);
+        }
+
+        public static bool CanPlaceAQueen(int[][] matrix, int row, int col)
+        {
+            if (matrix[row][col] != 0)
+            {
+                return false;
+            }
+
+            if (matrix[row].Sum() != 0)
+            {
+                return false;
+            }
+
+            var columnContainsQueen = false;
+            for (int i = 0; i < 8; i++)
+            {
+                if (matrix[i][col] != 0)
+                {
+                    columnContainsQueen = true;
+                }
+            }
+            if (columnContainsQueen)
+            {
+                return false;
+            }
+
+            var firstDiagonalContainsQueen = false;
+            var firstDiagonalRow = row;
+            var firstDiagonalCol = col;
+            while (firstDiagonalRow > 0 && firstDiagonalCol > 0)
+            {
+                firstDiagonalRow--;
+                firstDiagonalCol--;
+            }
+
+            while (firstDiagonalRow < 8 && firstDiagonalCol < 8)
+            {
+                if (matrix[firstDiagonalRow][firstDiagonalCol] != 0)
+                {
+                    firstDiagonalContainsQueen = true;
+                    break;
+                }
+                firstDiagonalRow++;
+                firstDiagonalCol++;
+            }
+
+            if (firstDiagonalContainsQueen)
+            {
+                return false;
+            }
+
+            var secondDiagonalContainsQueens = false;
+            var secondDiagonalRow = row;
+            var secondDiagonalCol = col;
+            while (secondDiagonalRow > 0 && secondDiagonalCol + 1 < 8)
+            {
+                secondDiagonalRow--;
+                secondDiagonalCol++;
+            }
+
+            while (secondDiagonalRow < 8 && secondDiagonalCol >= 0)
+            {
+                if (matrix[secondDiagonalRow][secondDiagonalCol] != 0)
+                {
+                    secondDiagonalContainsQueens = true;
+                    break;
+                }
+
+                secondDiagonalRow++;
+                secondDiagonalCol--;
+            }
+
+            if (secondDiagonalContainsQueens)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void PrintMatrix(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("-");
+                    }
+                    
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void PutQueens(int[][] matrix ,int row)
+        {
+            if (row == 8)
+            {
+                PrintMatrix(matrix);
+            }
+            else
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    if (CanPlaceAQueen(matrix,row, col))
+                    {
+                        matrix[row][col] = 1;
+                        PutQueens(matrix,row + 1);
+                        matrix[row][col] = 0;
+                    }
+                }
+            }
+        }
+    }
+   
+}
