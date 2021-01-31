5cdae32140562f28e489da0684b42ccefaae0b97
Svetlin <svetlimladenov@gmail.com>
2021-01-31 20:50:37 +0200

    SAVEPOINT

---
+++ 01.Recursion, Sorting and Searching Algorithms/Lab/SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortingAlgorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static int[] SelectionSort(int[] arr, int startIndex)
+        {
+            if (startIndex == arr.Length - 1)
+            {
+                return arr;
+            }
+            var minNumber = int.MaxValue;
+            var minNumberIndex = -1;
+            for (int i = startIndex; i < arr.Length; i++)
+            {
+                if (arr[i] < minNumber)
+                {
+                    minNumber = arr[i];
+                    minNumberIndex = i;
+                }
+            }
+
+            if (startIndex == minNumberIndex)
+            {
+                SelectionSort(arr, startIndex + 1);
+            }
+
+            var currentIndexNumber = arr[startIndex];
+            arr[startIndex] = minNumber;
+            arr[minNumberIndex] = currentIndexNumber;
+
+            return SelectionSort(arr, startIndex + 1);
+        }
+
+        public static int[] SelectionSortEasy(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var minNumber = arr[i];
+                var minNumberIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < minNumber)
+                    {
+                        minNumber = arr[j];
+                        minNumberIndex = j;
+                    }
+                }
+
+                var temp = arr[i];
+                arr[i] = minNumber;
+                arr[minNumberIndex] = temp;
+            }
+
+            return arr;
+        }
+
+        public static int[] BubleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        public static int[] InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var unsortedElement = arr[i];
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (arr[j] > unsortedElement)
+                    {
+                        //moves it one step back;
+                        var temp = arr[j];
+                        arr[j] = unsortedElement;
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+
+
+        private static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = unsorted.Count / 2;
+            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())  //Comparing First two elements to see which is smaller
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      //Rest of the list minus the first element
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ BitwiseOperators/BitwiseOperators/BitwiseOperators/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace BitwiseOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 0 - 0
+            // 1 - 1
+            // 2 - 10
+            // 3 - 11
+            // 4 - 100
+            // 5 - 101
+            // 6 - 110
+            // 7 - 111
+            // 8 - 1000
+            // 9 - 1001
+            // 10 - 1010
+            // 11 - 1011
+            // 12 - 1100
+            // 13 - 1101
+            // 14 - 1110
+            // 15 - 1111
+            // 16 - 10000
+            // 17 - 10001
+            // 18 - 10010
+            // 19 - 10011
+            // 20 - 10100
+            // 21 - 10101
+            // 22 - 10110
+            // 23 - 10111
+            // 24 - 11000
+            // 25 - 11001
+            // 1 + 8 + 16 = 25
+
+
+            var a = 10;    //  1010  //if 2 ones - 1   else 0
+            var b = 14;    //  1110
+            var c = a & b; //  1010
+
+            var first = 10;  // 1010
+            var second = 14;  //1110
+
+            var ili = first | second; // 1110
+
+            
+
+            var k = 10; //1010 // if same - 0 , if different - 1
+            var f = 14; //1110
+            var xor = k ^ f; // 0100 - 4
+
+
+
+            var twentyFive = 25; // 00011001  // move to the left 2 times - 1100100
+            var leftTwentyFive = twentyFive << 2;
+
+
+
+            var rightTwentyFive = twentyFive >> 3; // 11
+            Console.WriteLine(rightTwentyFive); 
+            Console.WriteLine(ConvertToBinary(rightTwentyFive));
+         }
+
+        public static string ConvertToBinary(int decimalNumber)
+        {
+            int remainder;
+            var result = string.Empty;
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                result = remainder.ToString() + result;
+            }
+
+            return result;
+        }
+
+        public static int ConvertToDecimalNumber(string binaryNumber)
+        {
+            var currentStepen = binaryNumber.Length - 1;
+            var backToDecimal = 0.0;
+
+            for (int i = 0; i < binaryNumber.Length; i++)
+            {
+                if (binaryNumber[i] == '0')
+                {
+                    currentStepen--;
+                    continue;
+                }
+                backToDecimal += Math.Pow(int.Parse(binaryNumber[i].ToString()) * 2, currentStepen);
+                currentStepen--;
+            }
+
+            return (int)backToDecimal;
+        }
+    }
+}
